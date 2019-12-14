a6dca7b137fbee020d0c15f2e011e96a8494bfec
Nick Welch <nwelch@treetopllc.com>
2019-12-14 00:09:30 +0000

    Basic start at implementation. POST/GET work.

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # usersvc_exercise
-Microservice built as a code exercise
+Microservice built as a code exercise.
+
+# How to Build and Run
+
+This project uses Python 3, so be careful not to accidentally use Python 2 if you have it installed.
+
+    virtualenv env # or maybe virtualenv-3 depending on your OS/package situation
+    source env/bin/activate
+    pip install -r requirements.txt
+    FLASK_APP=usersvc.py flask run
+
+# Features
+
+* A User rest Resource that allows clients to create, read, update, delete a user or a list of users.
+    * I found some ambiguity in this sentence: which parts need to support a list of users? Would you delete a list of
+      users? Update a list? Create a list? These seem a little unusual (but still plausible), so I would ideally get
+      clarification before implementing list operations, so I'm not building unwanted functionality. Until/unless I get
+      clarification, I'll assume a typical REST interface:
+        * `GET /users` lists all users
+	* `GET /users/:id` returns one user
+	* `POST/PATCH/DELETE /users/:id` operate on one user
+* For simplicity, the database will be a simple dict that is not persisted.
+* A user resource contains:
+    * id
+    * first name
+    * last name
+    * zip code
+    * email address
+* I intentionally omitted features that weren't requested, for example:
+    * Authentication
+    * Authorization
+    * Ordering/sorting of results
+    * Filtering
+    * Pagination
+    * API docs, e.g. Swagger
+* I did implement some input validation, because it's not too complicated, and accepting garbage data is pretty sad,
+  even in a toy project.
+
+# Design
+
+Normally I might've used something like the `flask_restplus` package, which helps streamline things like input
+validation and documentation generation, but I haven't used it before, so I stuck with vanilla Flask just to avoid
+dragging out this exercise too much.
+
+# Notes/Thoughts
+
+In a real user service, security and privacy would be top concerns, and would impact the design:
+
+* Would you ever really need to list all users in the database? Maybe not. I would be interested in omitting that
+  feature. The ability to list an entire database's worth of names, zips, and emails would be a target for attacks.
+* Maybe even consider rate limiting?
+* Authentication and Authorization would be important, to ensure that not just anyone can go snooping on other users'
+  information, or tampering with data.
+* More robust input validation would be a good thing.
---
+++ usersvc.py
@@ -0,0 +1,141 @@
+import re, uuid
+
+from flask import Flask, request, abort, url_for
+from flask.json import jsonify
+
+app = Flask(__name__)
+
+### Model and persistence
+
+class User:
+    # I would use a namedtuple to reduce boilerplate here, but my local Python isn't new enough.
+    def __init__(self, id, firstname, lastname, zipcode, email):
+        self.id = id
+        self.firstname = firstname
+        self.lastname = lastname
+        self.zipcode = zipcode
+        self.email = email
+
+    def to_dict(self):
+        return {
+            attr: getattr(self, attr)
+            for attr in ('id', 'firstname', 'lastname', 'zipcode', 'email')
+        }
+
+db = {}
+
+def debug_print_db(db):
+    print("----- Database dump -----")
+    print("{} users:".format(len(db)))
+    for u in db.values():
+        print("    {}".format(u.to_dict()))
+    print("----- End -----")
+
+### Input validation & request error handling
+
+class ValidationError(ValueError):
+    pass
+
+reasonable_length = 50
+
+zip_pattern = re.compile(r'^\d{5}(-\d{4})?$')
+
+# this is a crude sanity check. to fully parse and validate an email address is famously complicated:
+# http://www.ex-parrot.com/pdw/Mail-RFC822-Address.html
+email_pattern = re.compile(r'.+@.+')
+
+def validate_user(form):
+    if len(form.get('firstname', '')) > reasonable_length:
+        raise ValidationError("firstname too long")
+    if len(form.get('lastname', '')) > reasonable_length:
+        raise ValidationError("lastname too long")
+
+    zipcode = form.get('zipcode')
+    if zipcode:
+        if not zip_pattern.match(zipcode):
+            raise ValidationError("zipcode must be formatted as either NNNNN or NNNNN-NNNN")
+
+    email = form.get('email', '')
+    if len(email) > reasonable_length:
+        raise ValidationError("email too long")
+    print(email)
+    if not email_pattern.match(email):
+        raise ValidationError("invalid email")
+
+### Route handlers
+
+@app.route("/users/<id>", methods=["GET"])
+def get_user(id):
+    try:
+        return jsonify(db[id].to_dict())
+    except KeyError:
+        return abort(404)
+
+@app.route("/users", methods=["GET"])
+def list_users():
+    return jsonify(u.to_dict() for u in db.values())
+
+@app.route("/users", methods=["POST"])
+def create_user():
+    if not request.is_json:
+        return 'Request Content-Type must be application/json', 400
+
+    form = request.get_json()
+    try:
+        validate_user(form)
+    except ValidationError as e:
+        return str(e), 400
+
+    user = User(
+        id=str(uuid.uuid4()),
+        firstname=form.get('firstname', ''),
+        lastname=form.get('lastname', ''),
+        zipcode=form.get('zipcode', ''),
+        email=form.get('email', ''),
+    )
+    db[user.id] = user
+    debug_print_db(db)
+
+    # typical HTTP 201 Created response
+    headers = {'Location': url_for('get_user', id=id)}
+    return jsonify(user.to_dict()), 201, headers
+
+@app.route("/users/<id>", methods=["PATCH"])
+def update_user(id):
+    try:
+        user = db[id]
+    except KeyError:
+        return abort(404)
+
+    if not request.is_json:
+        return 'Request Content-Type must be application/json', 400
+    form = request.get_json()
+
+    try:
+        validate_user(form)
+    except ValidationError as e:
+        return str(e), 400
+
+    def maybe_update_field(user, form, attr):
+        # this eliminates some repetition, but in the real world this might lead to undesirable coupling between the
+        # attribute names exposed in the API and the attribute names in the model implementation -- they wouldn't always
+        # necessarily be the same.
+        if attr in form and form[attr] != getattr(user, attr):
+            setattr(user, attr, request.form[attr])
+
+    maybe_update_field(user, form, 'firstname')
+    maybe_update_field(user, form, 'lastname')
+    maybe_update_field(user, form, 'zipcode')
+    maybe_update_field(user, form, 'email')
+
+    db[id] = user
+
+    return '', 204 # HTTP 204 No Content. Returning 200 with a body would also be valid.
+
+@app.route("/users/<id>", methods=["DELETE"])
+def delete_user(id):
+    try:
+        del db[id]
+    except KeyError:
+        return abort(404)
+    return '', 204
